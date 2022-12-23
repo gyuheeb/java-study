fbd8710db67d4c1563197675ce101ee4f9bd6fd7
gyuheeb <gyuhee4529@naver.com>
2022-12-23 16:03:53 +0900

    java.lang #finish...

--- chapter04/src/main/java/chapter04/StringTest02.java
+++ chapter04/src/main/java/chapter04/StringTest02.java
@@ -1,30 +1,25 @@
 package chapter04;
 
 public class StringTest02 {
-
+	
 	public static void main(String[] args) {
-		//불변성 immutability
+		// c:\temp
+		System.out.println("c:\\temp");
 		
-		String s1="abc";
-		String s2="def";
-		String s3= s2;
+		System.out.println("\"HELLO\"");
+		//\t:tab
+		//\r:carriage return
+		//\n:newline
+		//\b:bell
 		
-		s2= s1.toUpperCase();
-		
-		String s4= s2.concat("??");
-		String s5= "!".concat(s2).concat("@");
-		System.out.println(s1+"\n"+s2+"\n"+s3+"\n"+s4+"\n"+s5);
+		System.out.print("hello\tworld\n");
+		System.out.println("hello\tworld");
 
 		
-		System.out.println(equalsHello("hello"));
-		
-		System.out.println(equalsHello(null));
+		//'
+		char c='\'';
+		System.out.println(c);
 	}
 
-	private static boolean equalsHello(String s) {
-		
-		
-		return "hello".equals(s);
-	}
 
 }
