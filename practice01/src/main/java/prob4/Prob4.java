b861a05dc5426f3413ec5e627514ef190c31f0ee
gyuheeb <gyuhee4529@naver.com>
2022-12-29 09:52:38 +0900

    ..

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -10,6 +10,8 @@ public class Prob4 {
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
 		int  size = text.length();
+		
+		
 		for(int i=1;i<=size;i++) {
 			for(int j=0;j<i;j++) {
 				System.out.print(text.charAt(j));
