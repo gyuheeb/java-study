6e74b4f2c669846a733210a01629ae485f2e18c3
gyuheeb <gyuhee4529@naver.com>
2022-12-30 20:30:22 +0900

    Stop...

--- network/src/main/java/chat/ChatServer.java
+++ network/src/main/java/chat/ChatServer.java
@@ -1,10 +1,90 @@
 package chat;
 
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+import echo.EchoRequestHandler;
+
 public class ChatServer {
-
-	public static void main(String[] args) {
+	public static final int PORT = 5000;
 	
+	public static void main(String[] args) {
+		
+		BufferedReader br =null;
+		PrintWriter pw =null;
+		Socket socket = null;
+		ServerSocket serverSocket =null;
+		Scanner scanner =null;
+		
+		
+		try {
+			
+			//1. 키보드 
+			scanner = new Scanner(System.in);
+			//2. socket 생성
+			serverSocket =new ServerSocket();
+			//3. 연결
+			serverSocket.bind(new InetSocketAddress("0.0.0.0",PORT),10);
+			log("start...[PORT :" + PORT + "]");
+			
+			
+			//4. reader/writer 생성
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			
+			//5. join 프로토클
+			System. out. print("닉네임>" ) ;
+			String nickname = scanner.nextLine () ;
+			pw.println( "join:"+ nickname);
+			pw.flush();
+					
+			//6. ChatClientReceiveThread 시작
+				
+		//7. 키보드 입력 처리
+		while ( true ) {
+			socket = serverSocket.accept();
+			new EchoRequestHandler(socket).start();
+			String inputMessage = br.readLine();
+			if(inputMessage.equals("quit")) {
+				System.out.println("종료하였습니다.");
+				break;
+			}
+			
+		System.out.println(nickname+" : ");
+		String input = scanner.nextLine ();
+		pw.write(inputMessage+"\n");
+		pw.flush();
+		}
+		}catch(SocketException e) {
+			System.out.println("[server] socket connect failed");
+		}
+		catch (IOException e) {
+		log("error : "+ e);
+		}finally {
+			try {
+				if(serverSocket != null && !serverSocket.isClosed()) {
+					scanner.close();
+			serverSocket.close();
+				}
+				
+			}catch(IOException ex){
+				log("error : "+ ex);
+			}
+		}
+		
+}
 
+	static void log(String message) {
+		System.out.println("[ChatServer ] " + message);
+		
 	}
-
 }
--- network/src/main/java/chat/ChatServerThread.java
+++ network/src/main/java/chat/ChatServerThread.java
@@ -1,5 +1,108 @@
 package chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 //port 마음대로
 public class ChatServerThread extends Thread {
+	private List<Writer> listWriters;
+	private String nickname;
+	private Socket socket;
+	BufferedReader br =null;
+	PrintWriter pw =null;
+	
+	public void CharServerThread(Socket socket, List<Writer> listWriters) {
+		this.socket =socket;
+		this.listWriters=listWriters;
+	//1. Remote Host Information
+	
+	//2. 스트림 얻기
+	BufferedReader in;
+	try {
+		 br = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+		pw = new PrintWriter( new OutputStreamWriter(socket.getOutputStream(),StandardCharsets.UTF_8));
+	
+		while(true) {
+			String request = br.readLine();
+			String[] tokens =request.split(" : ");
+			if("join".equals(tokens[0])) {
+				doJoin(tokens[1], pw);
+			} else if("message".equals(tokens[0])) {
+				doMessage(tokens[1]);
+			}else if("quit".equals(tokens[0])) {
+				doQuit(pw);
+			}
+			else {
+				ChatServer.log ("에러: 알수 없는 요청( " + tokens[0] + ")" );
+			}
+		}
+	} catch (IOException e) {
+			e.printStackTrace();
+	}
+	
+	
+	//3. 요청 처리 
+
+//			if(request ==null) {
+//				ChatServer.log("클라이언트로 부터 연결 끊김");
+//				doQuit(out);
+//				break;
+//			}
+		
+	//4. 프로토콜 분석
+	
+	}
+	
+	private void doJoin(String nickName, Writer writer) throws IOException {
+		this.nickname=nickName;
+		String data = nickName + "님이 참여하였습니다." ;
+		broadcast(data);
+		
+		/*writer pool에 저장*/
+		addWriter(writer);
+		
+		//ack
+		((PrintWriter) writer).println("join:ok");
+        writer.flush();
+		
+	}
+		private void addWriter(Writer writer) {
+			synchronized(listWriters) {
+				listWriters.add(writer);
+			}
+		}
+	
+		private void broadcast(String data) {
+			synchronized(listWriters) {
+				for(Writer writer:listWriters) {
+					PrintWriter printWriter = (PrintWriter)writer;
+					printWriter.println(data);
+					printWriter.flush();
+				}
+			}
+		}
+		private void doMessage (String message) throws IOException {
+			String data = nickname + " : "+message;
+			broadcast(data);
+		}
+	
+		private void doQuit(Writer writer) throws IOException{
+			removeWriter(writer);
+			String data = nickname +"님이 퇴장하였습니다.";
+			broadcast(data);
+		}
+	
+		private void removeWriter(Writer writer) {
+			listWriters.remove(pw);
+		}
+
+
 
 }
